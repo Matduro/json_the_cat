e53941ec3ad0af7788d940a9939a346023c9ecea
Mathieu Rousseau <mathieurousseau123@gmail.com>
2021-03-11 22:29:35 +0000

    JSON the cat

---
+++ breedFetcher.js
@@ -0,0 +1,241 @@
+const request = require('request'); // pull push info from the web
+
+
+
+// pull this: https://api.thecatapi.com/v1/breeds/search
+
+
+// const data = JSON.parse(body);
+// console.log(data);
+// console.log(typeof data);
+
+
+const fetchBreedDescription = function(breedName, callback) {
+
+  // const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=siberian`;
+  request(url, (error, response, body) => {
+  // console.log(body);
+  // console.log(typeof body);
+
+    if (error) {
+      callback(`Failed request: ${error}`, null);
+      return null;
+    }
+    if (response.statusCode !== 200) {
+      callback('Server responded with:' + response.statusCode, null);
+      return;
+    }
+
+    const data = JSON.parse(body);
+    // console.log(data);
+
+    const breed = data[0];
+    if (breed) {
+      callback(null, breed.description.trim());
+    } else {
+      callback(`Failed to find ${breedName}`, null);
+    }
+  });
+
+};
+
+console.log(fetchBreedDescription());
+
+module.exports = { fetchBreedDescription };
+/*
+[Function: request] {
+  get: [Function (anonymous)],
+  head: [Function (anonymous)],
+  options: [Function (anonymous)],
+  post: [Function (anonymous)],
+  put: [Function (anonymous)],
+  patch: [Function (anonymous)],
+  del: [Function (anonymous)],
+  delete: [Function (anonymous)],
+  jar: [Function (anonymous)],
+  cookie: [Function (anonymous)],
+  defaults: [Function (anonymous)],
+  forever: [Function (anonymous)],
+  Request: [Function: Request] {
+    debug: undefined,
+    defaultProxyHeaderWhiteList: [
+      'accept',           'accept-charset',
+      'accept-encoding',  'accept-language',
+      'accept-ranges',    'cache-control',
+      'content-encoding', 'content-language',
+      'content-location', 'content-md5',
+      'content-range',    'content-type',
+      'connection',       'date',
+      'expect',           'max-forwards',
+      'pragma',           'referer',
+      'te',               'user-agent',
+      'via'
+    ],
+    defaultProxyHeaderExclusiveList: [ 'proxy-authorization' ]
+  },
+  initParams: [Function: initParams],
+  debug: [Getter/Setter]
+}
+*/
+
+/*
+fs:
+{
+  appendFile: [Function: appendFile],
+  appendFileSync: [Function: appendFileSync],
+  access: [Function: access],
+  accessSync: [Function: accessSync],
+  chown: [Function: chown],
+  chownSync: [Function: chownSync],
+  chmod: [Function: chmod],
+  chmodSync: [Function: chmodSync],
+  close: [Function: close],
+  closeSync: [Function: closeSync],
+  copyFile: [Function: copyFile],
+  copyFileSync: [Function: copyFileSync],
+  createReadStream: [Function: createReadStream],
+  createWriteStream: [Function: createWriteStream],
+  exists: [Function: exists],
+  existsSync: [Function: existsSync],
+  fchown: [Function: fchown],
+  fchownSync: [Function: fchownSync],
+  fchmod: [Function: fchmod],
+  fchmodSync: [Function: fchmodSync],
+  fdatasync: [Function: fdatasync],
+  fdatasyncSync: [Function: fdatasyncSync],
+  fstat: [Function: fstat],
+  fstatSync: [Function: fstatSync],
+  fsync: [Function: fsync],
+  fsyncSync: [Function: fsyncSync],
+  ftruncate: [Function: ftruncate],
+  ftruncateSync: [Function: ftruncateSync],
+  futimes: [Function: futimes],
+  futimesSync: [Function: futimesSync],
+  lchown: [Function: lchown],
+  lchownSync: [Function: lchownSync],
+  lchmod: [Function: lchmod],
+  lchmodSync: [Function: lchmodSync],
+  link: [Function: link],
+  linkSync: [Function: linkSync],
+  lstat: [Function: lstat],
+  lstatSync: [Function: lstatSync],
+  lutimes: [Function: lutimes],
+  lutimesSync: [Function: lutimesSync],
+  mkdir: [Function: mkdir],
+  mkdirSync: [Function: mkdirSync],
+  mkdtemp: [Function: mkdtemp],
+  mkdtempSync: [Function: mkdtempSync],
+  open: [Function: open],
+  openSync: [Function: openSync],
+  opendir: [Function: opendir],
+  opendirSync: [Function: opendirSync],
+  readdir: [Function: readdir],
+  readdirSync: [Function: readdirSync],
+  read: [Function: read],
+  readSync: [Function: readSync],
+  readv: [Function: readv],
+  readvSync: [Function: readvSync],
+  readFile: [Function: readFile],
+  readFileSync: [Function: readFileSync],
+  readlink: [Function: readlink],
+  readlinkSync: [Function: readlinkSync],
+  realpath: [Function: realpath] { native: [Function (anonymous)] },
+  realpathSync: [Function: realpathSync] { native: [Function (anonymous)] },
+  rename: [Function: rename],
+  renameSync: [Function: renameSync],
+  rm: [Function: rm],
+  rmSync: [Function: rmSync],
+  rmdir: [Function: rmdir],
+  rmdirSync: [Function: rmdirSync],
+  stat: [Function: stat],
+  statSync: [Function: statSync],
+  symlink: [Function: symlink],
+  symlinkSync: [Function: symlinkSync],
+  truncate: [Function: truncate],
+  truncateSync: [Function: truncateSync],
+  unwatchFile: [Function: unwatchFile],
+  unlink: [Function: unlink],
+  unlinkSync: [Function: unlinkSync],
+  utimes: [Function: utimes],
+  utimesSync: [Function: utimesSync],
+  watch: [Function: watch],
+  watchFile: [Function: watchFile],
+  writeFile: [Function: writeFile],
+  writeFileSync: [Function: writeFileSync],
+  write: [Function: write],
+  writeSync: [Function: writeSync],
+  writev: [Function: writev],
+  writevSync: [Function: writevSync],
+  Dir: [class Dir],
+  Dirent: [class Dirent],
+  Stats: [Function: Stats],
+  ReadStream: [Getter/Setter],
+  WriteStream: [Getter/Setter],
+  FileReadStream: [Getter/Setter],
+  FileWriteStream: [Getter/Setter],
+  _toUnixTimestamp: [Function: toUnixTimestamp],
+  F_OK: 0,
+  R_OK: 4,
+  W_OK: 2,
+  X_OK: 1,
+  constants: [Object: null prototype] {
+    UV_FS_SYMLINK_DIR: 1,
+    UV_FS_SYMLINK_JUNCTION: 2,
+    O_RDONLY: 0,
+    O_WRONLY: 1,
+    O_RDWR: 2,
+    UV_DIRENT_UNKNOWN: 0,
+    UV_DIRENT_FILE: 1,
+    UV_DIRENT_DIR: 2,
+    UV_DIRENT_LINK: 3,
+    UV_DIRENT_FIFO: 4,
+    UV_DIRENT_SOCKET: 5,
+    UV_DIRENT_CHAR: 6,
+    UV_DIRENT_BLOCK: 7,
+    S_IFMT: 61440,
+    S_IFREG: 32768,
+    S_IFDIR: 16384,
+    S_IFCHR: 8192,
+    S_IFBLK: 24576,
+    S_IFIFO: 4096,
+    S_IFLNK: 40960,
+    S_IFSOCK: 49152,
+    O_CREAT: 512,
+    O_EXCL: 2048,
+    UV_FS_O_FILEMAP: 0,
+    O_NOCTTY: 131072,
+    O_TRUNC: 1024,
+    O_APPEND: 8,
+    O_DIRECTORY: 1048576,
+    O_NOFOLLOW: 256,
+    O_SYNC: 128,
+    O_DSYNC: 4194304,
+    O_SYMLINK: 2097152,
+    O_NONBLOCK: 4,
+    S_IRWXU: 448,
+    S_IRUSR: 256,
+    S_IWUSR: 128,
+    S_IXUSR: 64,
+    S_IRWXG: 56,
+    S_IRGRP: 32,
+    S_IWGRP: 16,
+    S_IXGRP: 8,
+    S_IRWXO: 7,
+    S_IROTH: 4,
+    S_IWOTH: 2,
+    S_IXOTH: 1,
+    F_OK: 0,
+    R_OK: 4,
+    W_OK: 2,
+    X_OK: 1,
+    UV_FS_COPYFILE_EXCL: 1,
+    COPYFILE_EXCL: 1,
+    UV_FS_COPYFILE_FICLONE: 2,
+    COPYFILE_FICLONE: 2,
+    UV_FS_COPYFILE_FICLONE_FORCE: 4,
+    COPYFILE_FICLONE_FORCE: 4
+  },
+  promises: [Getter]
+}
+*/
